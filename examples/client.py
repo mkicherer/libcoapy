e317ac819845d9454e67d74de11ffcf0d5884a5c
Mario Kicherer <dev@kicherer.org>
2024-07-04 00:00:00 +0200

    examples/client: get uri from commandline

--- examples/client.py
+++ examples/client.py
@@ -1,12 +1,17 @@
 
 from libcoapy import *
 
+if len(sys.argv) < 2:
+	uri_str = "coap://localhost"
+else:
+	uri_str = sys.argv[1]
+
 ctx = CoapContext()
 
-session = ctx.newSession("coap://localhost")
+session = ctx.newSession(uri_str)
 
 def rx_cb(session, tx_msg, rx_msg, mid):
-	print(rx_msg.bytes)
+	print(rx_msg.payload)
 	session.ctx.stop_loop()
 
 session.sendMessage(path=".well-known/core", response_callback=rx_cb)
