9eb883cd333cf3a55f146003539a291f06a0fd82
Mario Kicherer <mario.kicherer@sew-eurodrive.de>
2024-12-09 00:00:00 +0100

    genbindgen: update Makefile and libcoap.py.cfg

--- genbindgen/libcoap.cfg.py
+++ genbindgen/libcoap.cfg.py
@@ -1,6 +1,14 @@
 {
 	"clang_args": ["-DCOAP_API=", "-DCOAP_DEPRECATED=", "-DCOAP_STATIC_INLINE=",],
-	"opaque_types": ["coap_pdu_t"],
+	"opaque_types": ["coap_pdu_t", "coap_context_t",
+		"coap_resource_t", "coap_session_t", "coap_oscore_conf_t",
+		"coap_subscription_t", "coap_endpoint_t", "coap_socket_t",
+		"coap_async_t", "coap_cache_key_t", "coap_cache_entry_t", "coap_attr_t",
+		
+		
+		"fd_set", "epoll_event",
+		],
+	"include": { 2: "sockaddr" },
 	"functions": {
 		"coap_split_uri": {
 			3: ["in", "out"],
