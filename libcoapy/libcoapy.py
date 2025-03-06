5df0ffa77c812be8d460e8e68d3271c7cbc60f15
Mario Kicherer <mario.kicherer@sew-eurodrive.de>
2025-03-06 13:03:20 +0100

    add CoapObserverMultiplier

--- libcoapy/libcoapy.py
+++ libcoapy/libcoapy.py
@@ -712,27 +712,38 @@ class CoapClientSession(CoapSession):
 		else:
 			return await observer.__anext__()
 
+# This class is used to handle asynchronous requests. Besides requests with
+# observe flag set, this class also handles non-observe requests as the same
+# mechanisms are used in both cases.
 class CoapObserver():
-	def __init__(self, tx_pdu=None):
+	def __init__(self, tx_pdu=None, multiplier=None):
 		from asyncio import Event
 		
 		self.tx_pdu = tx_pdu
 		self.ev = Event()
 		self.rx_msgs = []
 		self._stop = False
+		# if stays false, this observer is used to return only a single response
 		self.observing = False
+		self.multiplier = multiplier
 	
 	def __del__(self):
 		self.stop()
 	
 	async def wait(self):
-		await self.ev.wait()
+		if self.multiplier:
+			await self.multiplier.process()
+		
+		# BUG for some reason, wait() returns True sometimes although is_set() immediately afterwards returns false
+		while not self.ev.is_set():
+			a = await self.ev.wait()
 	
 	def addResponse(self, rx_msg):
 		if self._stop:
 			return
 		
-		rx_msg.make_persistent()
+		if not self.multiplier:
+			rx_msg.make_persistent()
 		
 		self.rx_msgs.append(rx_msg)
 		
@@ -759,11 +770,39 @@ class CoapObserver():
 		if self._stop:
 			return
 		
-		if self.observing:
-			self.tx_pdu.cancelObservation()
+		if not self.multiplier:
+			if self.observing:
+				self.tx_pdu.cancelObservation()
+		else:
+			self.multiplier.removeSub(self)
 		
 		self._stop = True
 		self.ev.set()
+
+# This class enables multiple clients to asynchronously wait on a single subscribed
+# resource.
+class CoapObserverMultiplier():
+	def __init__(self, main_observer):
+		self.main_observer = main_observer
+		self.sub_observers = []
+		self.waiting = False
+	
+	def getSubObserver(self):
+		self.sub_observers.append( CoapObserver(multiplier=self) )
+		
+		return self.sub_observers[-1]
+	
+	def removeSub(self, sub):
+		self.sub_observers.remove(sub)
+	
+	async def process(self):
+		if self.waiting:
+			return
+		self.waiting = True
+		rx_pdu = await anext(self.main_observer)
+		for ob in self.sub_observers:
+			ob.addResponse(rx_pdu)
+		self.waiting = False
 
 class CoapEndpoint():
 	def __init__(self, ctx, uri):
